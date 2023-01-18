ae1ade7f5de2134023e87e2bb4ddda6a0fb606d9
CmBSinka <43815006+CmBSinka@users.noreply.github.com>
2023-01-18 12:05:47 +0300

    ServerBugFix

--- views/product/catalog.php
+++ views/product/catalog.php
@@ -12,6 +12,12 @@ use yii\grid\GridView;
 
 $this->title = 'Каталог товаров';
 $this->params['breadcrumbs'][] = $this->title;
+
+
+$categories=\app\models\Category::find()->all();
+
+
+
 echo "    <h1>Каталог товаров</h1>
   <style>
    .gty {
@@ -26,9 +32,30 @@ echo "    <h1>Каталог товаров</h1>
     <p><a href='https://pr-yanulyavichus.сделай.site/product/catalog?sort=name'>↑</a>По имени<a href='https://pr-yanulyavichus.сделай.site/product/catalog?sort=-name'>↓</a></p>
     <p><a href='https://pr-yanulyavichus.сделай.site/product/catalog?sort=country'>↑</a>По стране<a href='https://pr-yanulyavichus.сделай.site/product/catalog?sort=-country'>↓</a></p>
   </div>
+  
   </div>
-";
-$products=$dataProvider->getModels();
+";?>
+<div class="dropdown">
+    <a href="#" data-toggle="dropdown" class="dropdown-toggle">Label <b class="caret"></b></a>
+
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+            Выберите категорию
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <?php
+            $items='';
+            foreach ($categories as $category) {
+                $items .= " <li><a class='dropdown-item' href='/product/catalog?ProductSearh[category_id={$category->id}'>$category->categoria</a></li>";
+            }
+            echo $items;
+
+            ?>
+        </ul>
+    </div>
+
+</div>
+<?php $products=$dataProvider->getModels();
 echo "<div class='d-flex flex-row flex-wrap justify-content-start border border-1 border-info align-items-end'>";
 foreach ($products as $product){
 if ($product->count>0) {
